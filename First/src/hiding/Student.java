156adf6b05fa01d500baa92308ff9c22c14c9a94
dkgus89 <yuu1789@naver.com>
2023-04-29 21:01:53 +0900

    수요일꺼까지 완료!

---
+++ First/src/hiding/Student.java
@@ -0,0 +1,16 @@
+package hiding;
+
+public class Student {
+	int studentID;
+	private String studentName;		// studentName 변수를 private으로 선언
+	int grade;
+	String address;
+	
+	public String getStudentName() {	// private 변수인 studentName에 접근해 값을 가져오는 public get() 메서드
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {	// private 변수인 studentName에 접근해 값을 가져오는 public set() 메서드
+		this.studentName=studentName;
+	}
+}
