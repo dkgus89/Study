9408b5c4b59fd7b60413eef08c63e57a81eddec5
dkgus89 <yuu1789@naver.com>
2023-05-02 00:41:46 +0900

    55

---
+++ First/src/chapter5/Order.java
@@ -0,0 +1,12 @@
+package chapter5;
+
+public class Order {
+
+	long orderId;
+	String custormerId;
+	String orderDate;	// 날짜는 Calender 클래스를 많이 사용하지만, 여기서는 일단 String으로
+	String customerName;
+	String productId;
+	String shippingAddress;
+	
+}
