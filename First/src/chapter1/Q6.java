74389dbef3d392128936e43bc03fbf3454d042d3
dkgus89 <yuu1789@naver.com>
2023-05-01 12:58:23 +0900

    중간commit

---
+++ First/src/chapter1/Q6.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		System.out.println("윤아현");
+
+	}
+
+}
---
+++ First/src/chapter2/Q4.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class Q4 {
+	public static void main(String[] args) {
+		int a=10;
+		double b=2.0;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
+
+}
