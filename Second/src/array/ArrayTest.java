3fdae4be16d58a3b51fd63ad42f76980556fe8bc
dkgus89 <yuu1789@naver.com>
2023-05-03 15:43:41 +0900

    7장 완료!

--- Second/src/array/ArrayTest.java
+++ Second/src/array/ArrayTest.java
@@ -3,7 +3,17 @@ package array;
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] num=new int[] {1,2,3,4,5,6,7,8,9,10};
+		int sum=0;
+		
+		for(int i=0; i<num.length; i++) {
+			sum += num[i];	
+		}	
+		
+		System.out.println(sum);
+		//배열의 첫번째 요소(num[0])부터 열번째 요소(num[9])까지 10개 요소 값 출력
+		
 
 	}
 
